4efb62e055c5cbf959f508a35be8f5d5bf3f697e
Khawm Mung <johnmungtawng5@gmail.com>
2023-03-14 10:26:12 -0500

    rm mui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,6 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.css'
-import HikingIcon from '@mui/icons-material/Hiking';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
 
 function Navbar() {
     const [click, setClick] = useState(false);
@@ -13,11 +10,9 @@ function Navbar() {
     <nav className='navbar'>
         <div className='navbar-container'>
             <Link to="/" className='navbar-logo'>
-                KHLZN <HikingIcon color='primary' fontSize='medium'/>
+                KHLZN
             </Link>
             <div className='navbar-menu' onClick={handleClick}>
-                <MenuIcon click={true} />
-                <CloseIcon click={false} />
             </div>
         </div>
     </nav>
